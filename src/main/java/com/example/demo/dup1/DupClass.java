435d4f820ced5fbf05dddadc49621f8344802f80
Roller Wang <rwang@parasoft.com>
2023-03-03 15:08:09 +0800

    1

---
+++ src/main/java/com/example/demo/dup1/DupClass.java
@@ -0,0 +1,8 @@
+package com.example.demo.dup1;
+
+public class DupClass {
+
+    public void method() {
+        System.out.println("test Dup");
+    }
+}
---
+++ src/main/java/com/example/demo/dup2/DupClass.java
@@ -0,0 +1,8 @@
+package com.example.demo.dup2;
+
+public class DupClass {
+
+    public void method() {
+        System.out.println("test Dup");
+    }
+}
